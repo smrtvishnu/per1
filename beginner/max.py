85a1568ad46ecff75946982933d04f6d8b2e8d61
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-07-23 12:29:45 +0530

    Update max.py

--- beginner/max.py
+++ beginner/max.py
@@ -1,7 +1,4 @@
-a=[]
-n=int(input("Enter number of elements:"))
-for i in range(1,n+1):
-    b=int(input("Enter element:"))
-    a.append(b)
-a.sort()
-print("Largest element is:",a[n-1])
+>>> numbers = [20,67,3,2.6,7,74,2.8,90.8,52.8,4,3,2,5,7]
+>>> numbers.remove(max(numbers))
+>>> max(numbers)
+74
