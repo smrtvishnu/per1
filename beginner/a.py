7425292ac7a8499b468e3e6099997a9e8febcd06
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-07-23 12:08:09 +0530

    Update a.py

--- beginner/a.py
+++ beginner/a.py
@@ -1,11 +1,3 @@
-string=raw_input("Enter string:")
-char=0
-word=1
-for i in string:
-      char=char+1
-      if(i==' '):
-            word=word+1
-print("Number of words in the string:")
-print(word)
-print("Number of characters in the string:")
-print(char)
+sentence = 'Mary had a little lamb'
+>>> sentence.count('a')
+4
