18a9a8ec27e304d22f8a0b0bc3cc036dd24037ce
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-07-23 12:15:12 +0530

    Update b.py

--- beginner/b.py
+++ beginner/b.py
@@ -1,5 +1,12 @@
-a=input("Enter the value  ")
-
-for i in a:
-   if a[i]==(" "):
-       print("Space is at potion", i)
+fname = input("Enter file name: ")
+k = 0
+ 
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            for letter in i:
+                if(letter.isspace):
+                    k=k+1
+print("Occurrences of blank spaces:")
+print(k)
