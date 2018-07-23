c38d6e4ff0894ed0a5ceaad1c587480acfeb6460
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-07-23 12:20:18 +0530

    Update bb.py

--- beginner/bb.py
+++ beginner/bb.py
@@ -1,7 +1,2 @@
-fname = input("Enter file name: ")
-num_lines = 0
-with open(fname, 'r') as f:
-    for line in f:
-        num_lines += 1
-print("Number of lines:")
-print(num_lines)
+with open('data.txt') as f:
+    print sum(1 for _ in f)
