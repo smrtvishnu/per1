7f27cb3d4de1b88ab90745f07f908c4c7f02014a
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-08-01 15:43:34 +0530

    Update count.java

--- beginner/count.java
+++ beginner/count.java
@@ -1,16 +1,21 @@
-public class NumberDigits {
-
-    public static void main(String[] args) {
-
-        int count = 0, num = 3452;
-
-        while(num != 0)
-        {
-            // num = num/10
-            num /= 10;
-            ++count;
-        }
-
-        System.out.println("Number of digits: " + count);
-    }
+package SimplerPrograms;
+ 
+import java.util.Scanner;
+ 
+public class NumberofDigitsUsingWhile {
+    
+	private static Scanner sc;
+	
+	public static void main(String[] args) {
+		int Number, Count=0;
+		sc = new Scanner(System.in);		
+		System.out.println("\n Please Enter any Number: ");
+		Number = sc.nextInt();
+		
+		while(Number > 0) {
+			Number = Number / 10;
+			Count = Count + 1; 
+		}
+		System.out.format("\n Number of Digits in a Given Number = %d", Count);
+	}
 }
