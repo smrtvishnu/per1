aa37cffa7a8d0010660cee5dfb132992e4e8d2ad
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-08-01 16:19:09 +0530

    Create deletion.java

---
+++ pro/deletion.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+
+public class convertxtoydiff
+{	
+	public static void main (String[] args) 
+    {
+		String X,Y;
+		System.out.println("Please enter Strings");
+		Scanner sn= new Scanner(System.in);
+		X=sn.next();
+		Y=sn.next();		
+		int count=0;
+		char[] arr1=X.toCharArray();
+		char[] arr2=Y.toCharArray();
+		int j=arr1.length-1;
+		int k=arr2.length-1;
+		if(k<j)
+		{
+			count+=k;
+			k++;
+		}
+		for(int i=0;i<arr2.length;i++)
+		{
+			if(i>j)
+				count+=i;
+			else if(arr2[i]!=arr1[i])
+				count+=(arr2[i]-arr1[i]);						
+		}
+		System.out.println("Cost is "+ count);
+
+    }
+}
