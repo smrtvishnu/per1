2f3b534b58a6a0ecd8f78c93fad35f806a62ef95
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-08-01 15:48:20 +0530

    Create next.java

---
+++ beginner/next.java
@@ -0,0 +1,16 @@
+class Series{
+
+public static void main(String args[]){
+
+    for(int i=1; i<=10; i++){
+
+         System.out.println(i);
+    }
+
+    System.out.println(i);
+
+    for(int j=9; j>=0; j--){
+
+         System.out.println(j);        
+   }          
+} 
