52f510ec535098d790c67ff86c5cb3e446f9f660
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-08-13 15:48:06 +0530

    Update 2numbers.py

--- pro/2numbers.py
+++ pro/2numbers.py
@@ -1,13 +1,7 @@
-n=int(input())
-q=int(input())
-l1=[]
-for i in range(n):
-    a=int(input())
-    l1.append(a)
-for j in range(q):
-    c=0
-    u=int(input())
-    v=int(input())
-    for i in range(u,v):
-        c=c+int(l1[i])
-    print(c)
+n1,q1= map(int,input().split())
+N=[int(i) for i in input().split()]
+for _ in range(q1):
+    u,v=map(int,input().split())
+    print(sum(N[u-1:v]))
+    
+    
