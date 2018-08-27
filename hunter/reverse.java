0d2686e4b34243a582ad15a491ebbe27f12200fe
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-08-27 14:33:19 +0530

    Create reverse.java

---
+++ hunter/reverse.java
@@ -0,0 +1,10 @@
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("wipro technologies");
+		StringBuffer sb1=new StringBuffer("");
+		sb=sb.reverse();
+		System.out.println(sb);
+	}
+}
