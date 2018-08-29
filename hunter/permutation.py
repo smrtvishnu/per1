9891e392cec1ccbe216c5c55ca18451c5e184008
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-08-29 14:45:14 +0530

    Update permutation.py

--- hunter/permutation.py
+++ hunter/permutation.py
@@ -1,14 +1,5 @@
-# Python program to print all permutations with
-# duplicates allowed
- 
 def toString(List):
     return ''.join(List)
- 
-# Function to print permutations of string
-# This function takes three parameters:
-# 1. String
-# 2. Starting index of the string
-# 3. Ending index of the string.
 def permute(a, l, r):
     if l==r:
         print toString(a)
@@ -16,12 +7,8 @@ def permute(a, l, r):
         for i in xrange(l,r+1):
             a[l], a[i] = a[i], a[l]
             permute(a, l+1, r)
-            a[l], a[i] = a[i], a[l] # backtrack
- 
-# Driver program to test the above function
+            a[l], a[i] = a[i], a[l] 
 string = "ABC"
 n = len(string)
 a = list(string)
 permute(a, 0, n-1)
- 
-# This code is contributed by Bhavya Jain
