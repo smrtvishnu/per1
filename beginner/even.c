76a178dcbb7948d0160c2dc4b03fb75d8dd1941d
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-09-06 15:14:31 +0530

    Create even.c

---
+++ beginner/even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,n;
+printf("enter 2 number");
+scanf("%d%d",&a,&b);
+n=a*b;
+if(n%2==0)
+{
+printf("product is even");
+}
+else
+{
+printf("product is odd");
+}
+}
