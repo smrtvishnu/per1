0320b187b98e20f25f13170147072c3a9a0c6c2b
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-12-19 16:06:46 +0530

    Update 1prime.py

--- beginner/1prime.py
+++ beginner/1prime.py
@@ -1,13 +1,11 @@
-try:
-	n=int(input())
-	flag=0
-	for i in range(2,int(n/2)+1):
-		if n%i ==0:
-			flag=1
-			break
-	if flag==0:
-		print('prime')
-	else :
-		print('not prime')
-except:
-print('invalid')
+
+no=raw_input()
+flag=0
+for i in range(2,no):
+	if(no%i == 0):
+		flag=1
+		break
+if(flag!=1):
+	print("yes")
+else:
+	print("no")
