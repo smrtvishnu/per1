dc3df93fede02894d554c66c823e9d9eb67b129d
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-12-19 15:51:03 +0530

    Update minimum.py

--- beginner/minimum.py
+++ beginner/minimum.py
@@ -1,11 +1,7 @@
-num1 = 10
-num2 = 14
-num3 = 12
-if (num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
-else:
-   largest = num3
-
-print("The largest number between",num1,",",num2,"and",num3,"is",largest)
+def smallest_num_in_list( list ):
+    min = list[ 0 ]
+    for a in list:
+        if a < min:
+            min = a
+    return min
+print(smallest_num_in_list([1, 2, -8, 0]))
