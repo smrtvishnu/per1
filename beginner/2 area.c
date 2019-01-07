6733d1dc17b8dce1e2e22bda46d1a88b239523d6
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2019-01-07 11:45:41 +0530

    Update 2 area.c

--- beginner/2 area.c
+++ beginner/2 area.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+
+int main(void) 
 {
-    float l,b,a;
-    printf("enter b and l");
-    scanf("%f %f",&l,&b);
-    a=l*b;
-    printf("area of farm is=%.5f",a);
+	float l,b,a;
+	printf("enter the length and breadth values of the farm:");
+	scanf("%f %f",&l,&b);
+	a=l*b;
+	printf("\n%0.5f",a);
+	return 0;
 }
