215f6d827e5a82cac016f02f8ff5d4ec9fd658ae
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2019-01-08 10:51:51 +0530

    Create 2 oddeven.c

---
+++ beginner/2 oddeven.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[10];
+	int i,n;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%c",a[i]);
+		}
+		
+	}
+	printf("\t");
+	for(i=0;i<n;i++)
+	{
+		if(i%2!=0)
+		{
+			printf("%c",a[i]);
+		}
+		
+	}
+	
+	return 0;
+}
