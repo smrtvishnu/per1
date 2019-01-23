e4e67f634fb973fa15d70fce9d11aacc2567a2d2
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2019-01-23 12:12:25 +0530

    Update 3 floor.c

--- beginner/3 floor.c
+++ beginner/3 floor.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include<math.h>
+
 int main()
 {
-	float p,r,si;
-	int n,result;
-	scanf("%f %d %f",&p,&n,&r);
-	si=((p*n*r)/100);
-	result=floor(si);
-	printf("%d",result);
-	return 0;
+    int n,si,p,r;
+    scanf("%d %d %d",&n,&p,&r);
+    si=(p*n*r)/100;
+    printf("%d",si);
+    return 0;
 }
